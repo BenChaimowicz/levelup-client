3fb6d649b783707de5262959a8663c0d2b1954cc
Ben Chaimo <doroza@hotmail.com>
2020-06-21 23:24:09 +0300

    sign in design

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  isOpen = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  open() {
+    this.isOpen = true;
+  }
+  close() {
+    this.isOpen = false;
+  }
 }
